0a155f77c1f534296106235ac1133fd9dfbd630e
Austin Horner <Lyedar@wl-87-54.wireless.umt.edu>
2016-06-15 11:44:28 -0600

    Mongo URL Update

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ var webpack = require('webpack');
 var app = express();
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGOHQ_URL || 'mongodb://localhost/blog');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/blog');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
