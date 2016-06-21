8d8f463a6856a73060e0e5dddbb2011051185c3f
Mark Trepanier <Trep@wl-85-46.wireless.umt.edu>
2016-06-21 14:22:54 -0600

    Profile Page

--- app/routes.jsx
+++ app/routes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from 'containers/App';
-import AddPost from 'containers/AddPost';
+import Profile from 'containers/Profile';
 import Home from 'containers/Home';
 import SlugPost from 'containers/SlugPost';
 import ReadPage from 'containers/ReadPage';
@@ -18,7 +18,7 @@ export default (store) => {
   return (
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
-      <Route path="add-post" component={AddPost} />
+      <Route path="profile" component={Profile} />
       <Route path="post/:slug" component={SlugPost} />
       <Route path="read/:slug" component={ReadPage} />
       <Route path="login" component={Login} />
