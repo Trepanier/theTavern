a2a214b7edf244febc8da02f7e8d3481b7e355ab
Luke Walawander <LukeWalawander@mina-mays-macbook.local>
2016-06-21 14:31:50 -0600

    collection page access added

---
+++ app/containers/FrontCollection.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'whatwg-fetch';
+// import styles from 'css/components/FrontCollection';
+
+export default class FrontCollection extends React.Component {
+
+render () {
+	return (
+		<div>
+		<h1>Collections</h1>
+		</div>
+
+
+	)
+}
+};
