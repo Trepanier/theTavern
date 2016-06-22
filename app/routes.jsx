676996aafc9dcefca76a66d2a48f396ce669f9db
Luke Walawander <LukeWalawander@mina-mays-macbook.local>
2016-06-22 11:43:47 -0600

    added import search to routes.jsx as well as a search route path

--- app/routes.jsx
+++ app/routes.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
-import Profile from 'containers/Profile'
-import FrontCollection from 'containers/FrontCollection'
+import Search from 'containers/Search';
+import Profile from 'containers/Profile';
+import FrontCollection from 'containers/FrontCollection';
 import App from 'containers/App';
 import AddPost from 'containers/AddPost';
 import Home from 'containers/Home';
@@ -20,8 +21,9 @@ export default (store) => {
   return (
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
-      <Route path ="profile" component={Profile} />
-      <Route path ="collection" component={FrontCollection} />
+      <Route path="profile" component={Profile} />
+      <Route path="collection" component={FrontCollection} />
+      <Route path="search" component={Search} />
       <Route path="add-post" component={AddPost} />
       <Route path="post/:slug" component={SlugPost} />
       <Route path="read/:slug" component={ReadPage} />
