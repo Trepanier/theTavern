573b72ba9d2b24160305923d380f038e6298b123
Mark Trepanier <Trep@wl-85-46.wireless.umt.edu>
2016-06-22 13:34:15 -0600

    add item page works, now working on the add button

--- app/routes.jsx
+++ app/routes.jsx
@@ -9,6 +9,7 @@ import SlugPost from 'containers/SlugPost';
 import ReadPage from 'containers/ReadPage';
 import Login from 'containers/Login';
 import SignUp from 'containers/SignUp';
+import AddItem from 'containers/AddItem';
 
 /*
  * @param {Redux Store}
@@ -21,7 +22,7 @@ export default (store) => {
       <IndexRoute component={Home} />
       <Route path = "profile" component={Profile} />
       <Route path="collection" component={FrontCollection} />
-      
+      <Route path = "additem" component = {AddItem} />
       <Route path="post/:slug" component={SlugPost} />
       <Route path="read/:slug" component={ReadPage} />
       <Route path="login" component={Login} />
@@ -29,3 +30,6 @@ export default (store) => {
     </Route>
   );
 };
+
+
+
