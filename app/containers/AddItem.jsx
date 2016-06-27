0b3f9ecca535112e5da287a641de8fd87fbd83e7
Luke Walawander <LukeWalawander@mina-mays-macbook.local>
2016-06-27 16:09:19 -0600

    added visionController, added visionControllerpost to routes.js, and added vision infrastructure

--- app/containers/AddItem.jsx
+++ app/containers/AddItem.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-
 import classNames from 'classnames/bind';
 import styles from 'css/components/home';
 import 'whatwg-fetch';
-const cx = classNames.bind(styles);
 import {browserHistory} from 'react-router';
+const cx = classNames.bind(styles);
 
 /*
  * Note: This is kept as a container-level component, 
---
+++ server/controllers/visionController.js
@@ -0,0 +1,29 @@
+'use strict'
+const vision = require('node-cloud-vision-api')
+
+// init with auth
+vision.init({auth: 'AIzaSyCD_R3WCzQ6LEIX9N1Uw4F3u2ouNKt2TGU'})
+
+function scanImage() {
+	console.log('Scan Image Request Received andladsjf;alkdj')
+}
+// construct parameters
+const req = new vision.Request({
+  image: new vision.Image('VARIABLE TO PICTURE LOCATION'),
+  features: [
+    new vision.Feature('TEXT_DETECTION', 4)
+  ]
+})
+console.log("consolelog")
+// send single request
+vision.annotate(req).then((res) => {
+  // handling response
+  console.log("consolelog2")
+  console.log(JSON.stringify(res.responses))
+}, (e) => {
+  console.log('Error: ', e)
+}).catch((err) =>
+	console.log('Errrr', err)
+)
+
+module.exports = scanImage
