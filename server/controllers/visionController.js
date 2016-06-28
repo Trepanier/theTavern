86e7c03aab851216bb1474e346d93716309ac3e3
Luke Walawander <LukeWalawander@mina-mays-macbook.local>
2016-06-28 11:32:18 -0600

    added scanphoto branch and added uploads folder

--- server/controllers/visionController.js
+++ server/controllers/visionController.js
@@ -1,30 +1,26 @@
-// 'use strict'
-// const vision = require('node-cloud-vision-api')
+'use strict'
+const vision = require('node-cloud-vision-api')
 
-// // init with auth
-// vision.init({auth: 'AIzaSyCD_R3WCzQ6LEIX9N1Uw4F3u2ouNKt2TGU'})
+// init with auth
+vision.init({auth: 'AIzaSyCD_R3WCzQ6LEIX9N1Uw4F3u2ouNKt2TGU'})
 
-function scanImage() {
+function scanImage(requ,resp) {
 	console.log('Scan Image Request Received andladsjf;alkdj')
-	return {}
+	const req = new vision.Request({
+		image: new vision.Image(requ.file.path),
+		features: [
+		new vision.Feature('TEXT_DETECTION', 4)
+		]
+	})
+	// send single request
+	vision.annotate(req).then((res) => {
+  	// handling response
+  	console.log(JSON.stringify(res.responses))
+	}, (e) => {
+	console.log('Error: ', e)
+	}).catch((err) =>
+	console.log('Errrr', err)
+	)
 }
-// construct parameters
-// const req = new vision.Request({
-//   image: new vision.Image('/Users/minakomay/mtcs/myblog/app/images/MagicExample.jpg'),
-//   features: [
-//     new vision.Feature('TEXT_DETECTION', 4)
-//   ]
-// })
-// console.log("consolelog")
-// // send single request
-// vision.annotate(req).then((res) => {
-//   // handling response
-//   console.log("consolelog2")
-//   console.log(JSON.stringify(res.responses))
-// }, (e) => {
-//   console.log('Error: ', e)
-// }).catch((err) =>
-// 	console.log('Errrr', err)
-// )
 
 module.exports = {scanImage}
